a07c9a4f4f2cca0e7eaa9772ea1f49de6525d0c5
Arw0046 <128861661+Arw0046@users.noreply.github.com>
2023-04-11 08:31:55 -0400

    Chapter 5

---
+++ Chapter 5/1.c
@@ -0,0 +1,21 @@
+/*Write a program that calcualates how many digits a number contains.
+  You may assume that the number has no more than four digits.*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    int num = 0;
+
+    printf("Enter a number: ");
+    scanf("%4d", &num);
+
+    if(num <= 9) { printf("\nThe number %d has 1 digit", num); }
+    else if(num <= 99) { printf("\nThe number %d has 2 digits", num); }
+    else if(num <= 999) { printf("\nThe number %d has 3 digits", num); }
+    else if(num <= 9999) { printf("\nThe number %d has 4 digits", num); }
+    else printf("\nI didnt write enough code to figure that number out, sorry.");
+
+    return 0;
+}
