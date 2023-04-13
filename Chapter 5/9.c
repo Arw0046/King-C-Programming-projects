09026b9d9fb28a991854bb681d94a5f892c2073a
Arw0046 <128861661+Arw0046@users.noreply.github.com>
2023-04-13 08:41:01 -0400

    ch 5 q9

--- Chapter 5/9.c
+++ Chapter 5/9.c
@@ -7,6 +7,7 @@ int main() {
 
     int m1, d1, y1, m2, d2, y2;
     bool date1_is_smaller = true;
+    bool equal_dates = false;
 
     printf("Enter first date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &m1, &d1, &y1);
@@ -19,6 +20,7 @@ int main() {
         if(m1 < m2) {}
         else if(m1 == m2) {
             if(d1 < d2) {}
+            else if(d1 == d2) { date1_is_smaller = false; equal_dates = true; }
             else date1_is_smaller = false;
         } 
         else date1_is_smaller = false;
@@ -26,7 +28,10 @@ int main() {
     else date1_is_smaller = false;
 
     if(date1_is_smaller) {printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d ", m1, d1, y1, m2, d2, y2); }
-    else printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d ", m2, d2, y2, m1, d1, y1);
+    else if(!date1_is_smaller) { 
+        if(!equal_dates) printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d ", m2, d2, y2, m1, d1, y1);
+        else printf("Both dates are equal!");
+    }
 
    return 0;
 
