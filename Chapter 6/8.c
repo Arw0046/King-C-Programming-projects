cd937df791824049d7e65484b9e1ddf1782429e8
Arw0046 <128861661+Arw0046@users.noreply.github.com>
2023-04-15 19:21:05 -0400

    ch6 q8

---
+++ Chapter 6/8.c
@@ -0,0 +1,27 @@
+/*Write a program that prints a one month calender. the user specifies the number of days
+  in the month and the day of the week on which the calender begins.*/
+
+#include <stdio.h>
+
+int main() {
+
+    int days, starting_day;
+
+    printf("Enter number of days in month: ");
+    scanf("%d", &days);
+    printf("Enter starting day of the week(1=Sun, 7=Sat): ");
+    scanf("%d", &starting_day);
+
+    for (int i =1; i < starting_day; ++i) printf("   ");
+
+    for (int i = 1, day_of_week = starting_day; i <= days; ++i) {
+        if (day_of_week <= 7) { 
+            printf("%2d ", i);
+            ++day_of_week;
+        }
+        else { printf("\n%2d ", i); day_of_week = 2; }
+    }
+
+    return 0;
+
+}
