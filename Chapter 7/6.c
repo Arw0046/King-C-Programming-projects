6eee54d0ad80ade0e73503220db834d60cfbe68e
Arw0046 <128861661+Arw0046@users.noreply.github.com>
2023-04-21 14:14:20 -0400

    ch7 q6

---
+++ Chapter 7/6.c
@@ -0,0 +1,17 @@
+/*Write a program thst prints the values of sizeof(int), sizeof(short),
+  sizeof(long), sizeof(float), sizeof(double), and sizeof(long double).*/
+
+#include <stdio.h>
+
+int main() {
+
+int c;
+    printf("sizeof(int) = %lu\n", sizeof(c));
+    printf("sizeof(short) = %lu\n", sizeof(short));
+    printf("sizeof(long) = %lu\n", sizeof(long));
+    printf("sizeof(float) = %lu\n", sizeof(float));
+    printf("sizeof(double) = %lu\n", sizeof(double));
+    printf("sizeof(long double) = %lu\n", sizeof(long double));
+
+    return 0;
+}
